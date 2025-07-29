7c3d92ef5859e99f6433981cd03551aa87981cb8
Lucas Vasconcelos <manoellucacv@gmail.com>
2025-07-29 05:41:04 -0400

    Winner calc. properly | displays properly
    Need to look into random num

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
-function getHumanChoice()
-{
+function getHumanChoice() {
     return prompt("What do you choose?");
 }
 
-function playRound(humanChoiceInput, computerChoiceInput)
+function getComputerChoice()
 {
+    let randNum = Math.random;
+
+    if (randNum <= (randNum/3))
+        return "rock";
+    else if ((randNum/3) < randNum <= (randNum * 2 / 3))
+        return "paper";
+    else
+        return "scissors";
+}
+
+function playRound(humanChoiceInput, computerChoiceInput) {
     let humanChoice = humanChoiceInput.toLowerCase();
     let computerChoice = computerChoiceInput.toLowerCase();
 
-    if (humanChoice == "rock")
+    const beats =
     {
-        switch (computerChoice)
-        {
-            case "rock":
-                return "It's a tie!";
-                break;
-            case "paper":
-                computerScore++;
-                return "You Lose!"
-                break;
-            case "scissors":
-                humanScore++;
-                return "You Win!"
-                break;
-            default:
-                return "THERE WAS AN ERROR!!! LINE 27"
-        }
+        rock: 'scissors',
+        paper: 'rock',
+        scissors: 'paper'
     }
+
+    if (humanChoice == computerChoice)
+        return "It's a draw!"
+    else if (beats[humanChoice] == computerChoice) {
+        humanScore++;
+        return "You Won!";
+    }
+    else {
+        computerScore++;
+        return "You Lost!";
+    }
+
+    f
 }
 
 let humanScore = 0;
 let computerScore = 0;
+for (let i = 0; i < 10; i++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
 
-let humanChoice = getHumanChoice();
-
-console.log(playRound(humanChoice, "Rock"));
-console.log(humanScore + " ", + computerScore);
-
-console.log(playRound(humanChoice, "pAper"));
-console.log(humanScore + " ", + computerScore);
-
-console.log(playRound(humanChoice, "Scissors"));
-console.log(humanScore + " ", + computerScore);
+    console.log(playRound(humanChoice, computerChoice));
+    console.log(computerChoice);
+    console.log(humanScore + " ", computerScore);
+    
+}
