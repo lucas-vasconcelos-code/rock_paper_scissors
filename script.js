dd175ad936e0c0970cbc2df0e88da31419536e66
Lucas Vasconcelos <manoellucacv@gmail.com>
2025-07-29 05:18:23 -0400

    Calculated winner if human chose rock | disp. win

--- script.js
+++ script.js
@@ -3,4 +3,42 @@ function getHumanChoice()
     return prompt("What do you choose?");
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoiceInput, computerChoiceInput)
+{
+    let humanChoice = humanChoiceInput.toLowerCase();
+    let computerChoice = computerChoiceInput.toLowerCase();
+
+    if (humanChoice == "rock")
+    {
+        switch (computerChoice)
+        {
+            case "rock":
+                return "It's a tie!";
+                break;
+            case "paper":
+                computerScore++;
+                return "You Lose!"
+                break;
+            case "scissors":
+                humanScore++;
+                return "You Win!"
+                break;
+            default:
+                return "THERE WAS AN ERROR!!! LINE 27"
+        }
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+let humanChoice = getHumanChoice();
+
+console.log(playRound(humanChoice, "Rock"));
+console.log(humanScore + " ", + computerScore);
+
+console.log(playRound(humanChoice, "pAper"));
+console.log(humanScore + " ", + computerScore);
+
+console.log(playRound(humanChoice, "Scissors"));
+console.log(humanScore + " ", + computerScore);
